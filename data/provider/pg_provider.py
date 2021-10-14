31a02666e38aa6fca12edaf349062b1a29e935e3
Artem Dybov <xivrussian@gmail.com>
2021-10-14 14:25:15 +0300

    pg provider added

---
+++ data/provider/pg_provider.py
@@ -0,0 +1,28 @@
+from data.provider.database_provider import DBProvider
+import psycopg2
+
+
+class PGProvider(DBProvider):
+    def __init__(self):
+        super().__init__()
+        self.__connection__ = None
+        self.__cursor__ = None
+
+    def connect(self, host=None, dbname=None, user=None, password=None, port=None, sslmode=None):
+        self.__connection__ = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port,
+                                               sslmode=sslmode)
+        self.__cursor__ = self.__connection__.cursor()
+
+    def execute(self, sql, *args):
+        self.__cursor__.execute(sql, *args)
+
+    def get_cursor(self):
+        return self.__cursor__
+
+    def commit(self):
+        self.__connection__.commit()
+
+    def disconnect(self):
+        if self.__connection__ is not None:
+            self.__connection__.close()
+            self.__cursor__.close()
---
+++ main.py
@@ -0,0 +1,19 @@
+from data.provider.pg_provider import PGProvider
+
+db = PGProvider()
+db.connect(host="rc1b-3t64ginmv5ottmzv.mdb.yandexcloud.net", port=6432, sslmode=None, dbname="rebux",
+           user="lavrov",
+           password="relibrebux")
+
+db.execute('''CREATE TABLE "circulation" (
+     "circulation_id"	INTEGER,
+     "catalogue"	INTEGER,
+     "barcode"	INTEGER,
+     "startdate"	INTEGER,
+     "finishdate"	INTEGER,
+     "reader_id"	INTEGER,
+     "bookpoint_id"	INTEGER,
+     "state"	TEXT
+);''')
+
+db.commit()
