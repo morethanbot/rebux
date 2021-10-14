a36e93d7f9ded0f05e765f8d0653b245141318d5
ortemij <artyemlavrov39@gmail.com>
2021-10-14 19:12:02 +0300

    Работа над алгоритмом завершена.

--- bloc/entry_bloc.py
+++ bloc/entry_bloc.py
@@ -11,14 +11,14 @@ def add_entry(user_id: str, book_id: str):
     circulation_repository.add_user_book(user=user, book=book)
 
     user_books = circulation_repository.get_user_books(user=user)
-    user_book_ids = List[str]
+    user_book_ids = List[str]()
     for connected_book in user_books:
-        user_book_ids.append(__object=connected_book.book_id)
+        user_book_ids.append(connected_book.book_id)
         if connected_book.book_id != book.book_id:
             connection = Connection(book_from=book, book_to=connected_book, weight=1)
             connections_repository.update_connection(connection=connection)
 
-    recommendations = List[Recommendation]
+    recommendations = List[Recommendation]()
     for book in user_books:
         connections = connections_repository.get_connections(book_from=book)
         for connection in connections:
@@ -26,4 +26,4 @@ def add_entry(user_id: str, book_id: str):
                 recommendation = Recommendation(book=connection.book_to, user=user, confidence=connection.weight)
                 recommendations.append(recommendation)
     recommendations.sort(key=lambda x: x.confidence, reverse=True)
-
+    recommendations_repository.update_recommendations(user=user, recommendations=recommendations)
